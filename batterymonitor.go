a7708914cc68930c1883e9ae878750794ca6153b
hejinlai <hejinlai@foxmail.com>
2017-05-22 16:02:16 +0800

    v2.0
    Change-Id: I1c6ff4d75e5b8bc4763e59c191db63c1d085232e

--- batterymonitor.go
+++ batterymonitor.go
@@ -1,11 +1,16 @@
-package main
+package batterymonitor
 
+// package name smartconn.cc/sibolwolf/batterymonitor
 import (
-    "fmt"
-    "os"
     "log"
+    "os"
+    "os/exec"
+    "time"
     "strings"
+    "strconv"
     "golang.org/x/sys/unix"
+    SysSW   "smartconn.cc/sibolwolf/syssleepwake"
+    SysSWWH "smartconn.cc/sibolwolf/syssleepwake/wakehandle"
 )
 
 /*
@@ -14,7 +19,7 @@ var str_b string = "change@/devices/platform/axp22_board/axp22-supplyer.20/power
 
 var str_f string = "/devices/platform/axp22_board/axp22-supplyer.20/power_supply/battery"
 
-var batterpar = []string {
+var batterypar = []string {
     "ACTION",
     "DEVPATH",
     "SUBSYSTEM",
@@ -36,35 +41,109 @@ var batterpar = []string {
 }
 
 var batterystatus map[string]string
+var lastchargestatusint int
+var currbatterycapacityint int
+var lasttime int64
+var currtime int64
+var timeperiod int64 = 3 // second
+
 
 func BatteryStatusHandle(str_b string) {
+    // wake up handle
+    SysSWWH.WakeJudgment()
+    
 	batterystatus := make(map[string]string)
     if strings.Contains(str_b, str_f) {
         log.Println("This is the string I want")
         var start, stop int
         var par []string
-        for i := 0; i < len(batterpar)-1; i++ {
-            start = strings.Index(str_b, batterpar[i])
-            stop  = strings.Index(str_b, batterpar[i+1])
+        for i := 0; i < len(batterypar)-1; i++ {
+            start = strings.Index(str_b, batterypar[i])
+            stop  = strings.Index(str_b, batterypar[i+1])
             par = strings.Split(str_b[start:stop], "=")
-            log.Println("start", start)
-            log.Println("stop", stop)
-            log.Println("par", par)
             batterystatus[par[0]] = par[1]
         }
 
-        chargestatus := batterystatus[batterpar[7]]
+        chargestatus := batterystatus[batterypar[7]]
         batterycapacity := batterystatus[batterypar[15]]
+        chargestatus = strings.Replace(chargestatus, "\x00", "", -1)
+        batterycapacity = strings.Replace(batterycapacity, "\x00", "", -1)
 
-        log.Println("chargestatus", chargestatus)
-        log.Println("batterycapacity", batterycapacity)
+        // charge status handle
+        chargestatusint, chargestatus_err := strconv.Atoi(chargestatus)
+        if chargestatus_err != nil {
+            log.Println(chargestatus_err)
+        }
+
+        if chargestatusint != lastchargestatusint {
+            log.Println("Current battery charge status is:", chargestatusint)
+            currtime = time.Now().Unix()
+            if (currtime - lasttime) > timeperiod {
+                lasttime = currtime
+                lastchargestatusint = chargestatusint
+                SysSW.UpdateLockStatus("chargelock", chargestatusint)
+            } else {
+                lasttime = currtime
+                lastchargestatusint = chargestatusint
+                SysSW.UpdateLockStatus("chargelock", chargestatusint)
+            }
+        }
+
+        // battery capacity handle
+        batterycapacityint, batterycapacityint_err := strconv.Atoi(batterycapacity)
+        if batterycapacityint_err != nil {
+            log.Println(batterycapacityint_err)
+        } else {
+            currbatterycapacityint = batterycapacityint
+            log.Println("Current battery capacity is:", currbatterycapacityint)
+        }
+
 
     }
 }
 
-func Init() {
+func InitBatteryOnlineStatus() {
+    // Init battery online status
+    cmd := exec.Command("/bin/sh", "-c", "sysint getBatteryOnline")
+    bytes, err := cmd.Output()
+    if err != nil {
+        log.Println("Init battery online status fault: " + err.Error())
+    }
+
+    lasttime = time.Now().Unix()
+    chargestatus := strings.TrimSpace(string(bytes))
+    chargestatusint, err := strconv.Atoi(chargestatus)
+    if err != nil {
+        log.Println(err)
+    } else {
+        lastchargestatusint = chargestatusint
+        log.Println("Current battery charge status is:", chargestatusint)
+        SysSW.UpdateLockStatus("chargelock", lastchargestatusint)
+    }
+}
+
+func InitBatteryCapacity() {
+    // Init battery capacity_now
+    cmd := exec.Command("/bin/sh", "-c", "sysint getBatteryCapacity")
+    bytes, err := cmd.Output()
+    if err != nil {
+        log.Println("Init battery capacity now fault: " + err.Error())
+    }
+
+    batterycapacity := strings.TrimSpace(string(bytes))
+    batterycapacityint, err := strconv.Atoi(batterycapacity)
+    if err != nil {
+        log.Println(err)
+    } else {
+        currbatterycapacityint = batterycapacityint
+        log.Println("Current battery capacity is:", currbatterycapacityint)
+    }
+
+}
+
+func BatteryNetlinkMonitor() {
     // To communicate with netlink, a netlink socket must be opened. This is done using the socket() system call:
-    fmt.Println("Hello, this is netlink socket")
+    //log.Println("Hello, this is battery monitor")
     fd, socket_err := unix.Socket(
         // Always used when opening netlink sockets.
         unix.AF_NETLINK,
@@ -76,11 +155,8 @@ func Init() {
         unix.NETLINK_KOBJECT_UEVENT,
     )
 
-    fmt.Println("unix.NETLINK_KOBJECT_UEVENT:", unix.NETLINK_KOBJECT_UEVENT)
-    fmt.Println("fd is:", fd)
-
     if socket_err != nil {
-        fmt.Println("Socket_err is:" + socket_err.Error())
+        log.Println("Socket_err is:" + socket_err.Error())
     }
 
     // Once the socket is created, bind() must be called to prepare it to send and receive messages.
@@ -98,28 +174,40 @@ func Init() {
     })
 
     if bind_err != nil {
-        fmt.Println("Bind_err is:" + bind_err.Error())
+        log.Println("Bind_err is:" + bind_err.Error())
     }
 
     bstore := make([]byte, os.Getpagesize())
-    fmt.Println("length b is:", len(bstore))
+
     for {
-        fmt.Println("Start reading2 ...")
         for {
             // Peek at the buffer to see how many bytes are available.
             n, _, _ := unix.Recvfrom(fd, bstore, 4096)
-            fmt.Println("Length of data is:", n)
+            //log.Println("Length of data is:", n)
             // http://stackoverflow.com/questions/14230145/what-is-the-best-way-to-convert-byte-array-to-string
-            fmt.Println(string(bstore[:n]))
+            //log.Println(string(bstore[:n]))
             // Break when we can read all messages.
             if n < len(bstore) {
-                BatteryStatusHandle(bstore[:n])
+                BatteryStatusHandle(string(bstore[:n]))
                 break
             }
             // Double in size if not enough bytes.
             bstore = make([]byte, len(bstore)*2)
         }
-        fmt.Println("Start reading3 ...")
     }
+}
 
+func GetBatteryOnlineStatus() int {
+    return lastchargestatusint
+}
+
+func GetBatteryCapacityStatus() int {
+    return currbatterycapacityint
+}
+
+func Init() {
+    // Init battery online status
+    InitBatteryOnlineStatus()
+    InitBatteryCapacity()
+    BatteryNetlinkMonitor()
 }
